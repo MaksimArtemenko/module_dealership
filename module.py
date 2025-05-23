f82505120e1c39f784762d0ca24311ceec6c55e9
MaksimArtemenko <24fmif.m.artemenko@std.udu.edu.ua>
2025-05-23 21:13:51 +0300

    Check data for class Dealership

--- module.py
+++ module.py
@@ -1,9 +1,17 @@
 class Dealership:
-    def __init__ (self, dealership_name, dealership_ID):
+    def __init__(self, dealership_name: str, dealership_ID: int):
+        if not isinstance(dealership_name, str):
+            raise ValueError('dealership_name має бути рядком')
         self.dealership_name = dealership_name
+
+        if not isinstance(dealership_ID, int):
+            raise ValueError('dealership_ID має бути цілим числом')
         self.dealership_ID = dealership_ID
+        if not isinstance(self.dealerships, list):
+            raise ValueError('dealerships має бути списком')
+        self.dealerships = []
 class Car:
-    def __init__ (self, brand: str, model: str, years: int, type: str, purchasePrice: int, sellPrice: int, gearbox: str,
+    def __init__(self, brand: str, model: str, years: int, type: str, purchasePrice: int, sellPrice: int, gearbox: str,
              fuelType: str, colors: str):
         self.brand = brand
         self.model = model
@@ -16,10 +24,10 @@ class Car:
         self.colors = colors
 class Client:
     def __init__ (self, client_name: str, last_name: str, email: str, phoneNumber: int, clientID: int):
-        self.client_name = client_name
-        self.last_name = last_name
-        self.email = email
-        self.phoneNumber = phoneNumber
+        self.__client_name = client_name
+        self.__last_name = last_name
+        self.__email = email
+        self.__phoneNumber = phoneNumber
         self.clientID = clientID
 class Operation:
     pass
