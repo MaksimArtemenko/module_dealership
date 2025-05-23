2d49851f17f94d9d2fb7639a7ea243b5a55d58a2
lab4 <24fmif.o.machok@std.udu.edu.ua>
2025-05-23 20:53:11 +0300

    add data

--- module.py
+++ module.py
@@ -1,8 +1,25 @@
 class Dealership:
-    pass
+    def __init__ (self, dealership_name, dealership_ID):
+        self.dealership_name = dealership_name
+        self.dealership_ID = dealership_ID
 class Car:
-    pass
+    def __init__ (self, brand: str, model: str, years: int, type: str, purchasePrice: int, sellPrice: int, gearbox: str,
+             fuelType: str, colors: str):
+        self.brand = brand
+        self.model = model
+        self.years = years
+        self.type = type
+        self.purchasePrice = int
+        self.sellPrice = int
+        self.gearbox = gearbox
+        self.fuelType = fuelType
+        self.colors = colors
 class Client:
-    pass
+    def __init__ (self, client_name: str, last_name: str, email: str, phoneNumber: int, clientID: int):
+        self.client_name = client_name
+        self.last_name = last_name
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.clientID = clientID
 class Operation:
     pass
